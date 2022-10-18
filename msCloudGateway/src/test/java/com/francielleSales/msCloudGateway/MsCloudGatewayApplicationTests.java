fb1a0406b035841473154d9c3d1346b909fb585d
Francielle Sales <francielle_sales@hotmail.com>
2022-10-18 18:39:53 -0300

    Cloud gateway

---
+++ msCloudGateway/src/test/java/com/francielleSales/msCloudGateway/MsCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francielleSales.msCloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
