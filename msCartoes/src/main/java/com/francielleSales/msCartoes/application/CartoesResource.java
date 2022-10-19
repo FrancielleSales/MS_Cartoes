71ef429b0eb01635b6221e2a0d211c987afa649f
Francielle Sales <francielle_sales@hotmail.com>
2022-10-19 10:42:10 -0300

    Criacao e configuracao msCards

---
+++ msCartoes/src/main/java/com/francielleSales/msCartoes/application/CartoesResource.java
@@ -0,0 +1,9 @@
+package com.francielleSales.msCartoes.application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("cartoes")
+public class CartoesResource {
+}
